70f4cb359d03897ce53c2abf774a30ce22b0dc2a
sadahiro <sadahiro0310@gmail.com>
2021-05-18 21:28:36 +0900

    bz gem create

--- lib/bz.rb
+++ lib/bz.rb
@@ -2,5 +2,18 @@ require "bz/version"
 
 module Bz
   class Error < StandardError; end
-  # Your code goes here...
+  require 'yaml'
+  SINGLES = YAML.load_file('lib/bz_single.yml')
+
+  def release_year(song)
+    SINGLES.select{|i| i["title"].eql?(song)}.first["year"]
+  end
+
+  def self.all_titles
+    SINGLES.map{ |i| i["title"] }
+  end
+
+  def this_year_songs(year)
+    SINGLES.group_by { |i| i["year"] }[year]
+  end
 end
